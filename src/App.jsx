9ef62779c220559e12a44373d90a802ea9332b2d
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-09-06 01:26:58 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <main>
+      React ⚛️ + Vite ⚡
+      <br />
+      <a href="https://amukh1.dev">website</a>
+    </main>
+  )
+}
