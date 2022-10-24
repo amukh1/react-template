d844fbc0e5652200119c17860dc0eaf9e29dedca
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-10-24 01:37:15 -0400

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import './App.css'
 
+import {
+  HashRouter as Router,
+  Route,
+  Routes,
+  useLocation
+} from "react-router-dom";
+import { useEffect, useState, useRef } from "react";
+// import { withRouter } from "react-router";
+
 export default function App() {
   return (
     <main>
-      React ⚛️ + Vite ⚡
-      <br />
-      <a href="https://amukh1.dev">website</a>
+      <nav>
+      <Router>
+        <Routes>
+          <Route path="/" element={<div>hi</div>} />
+        </Routes>
+      </Router>
+      </nav>
     </main>
   )
 }
